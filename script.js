186b27b68aa5a30487850e542b35f8ebca2291cd
Umang Hirani <umanghirani.exe@gmail.com>
2024-04-02 13:48:23 +0530

    transition-from-right-to-left (with blur and opacity fades)

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
 input_div = document.getElementById("input-div");
+var input_div = document.getElementById('input-div');
+var rightSideDiv = document.getElementById('side-div right-side');
+var leftSideDiv = document.getElementById('side-div left-side');
 top1000Words = [
     "the", "of", "to", "and", "a", "in", "is", "it", "you", "that", "he", "was", "for", "on", "are", "with", "as", "I", "his", "they", "be", "at", "one", "have", "this", "from", "or", "had", "by", "hot", "but", "some", "what", "there", "we", "can", "out", "other", "were", "all", "your", "when", "up", "use", "word", "how", "said", "an", "each", "she", "which", "do", "their", "time", "if", "will", "way", "about", "many", "then", "them", "would", "write", "like", "so", "these", "her", "long", "make", "thing", "see", "him", "two", "has", "look", "more", "day", "could", "go", "come", "did", "my", "sound", "no", "most", "number", "who", "over", "know", "water", "than", "call", "first", "people", "may", "down", "side", "been", "now", "find", "any", "new", "work", "part", "take", "get", "place", "made", "live", "where", "after", "back", "little", "only", "round", "man", "year", "came", "show", "every", "good", "me", "give", "our", "under", "name", "very", "through", "just", "form", "much", "great", "think", "say", "help", "low", "line", "before", "turn", "cause", "same", "mean", "differ", "move", "right", "boy", "old", "too", "does", "tell", "sentence", "set", "three", "want", "air", "well", "also", "play", "small", "end", "put", "home", "read", "hand", "port", "large", "spell", "add", "even", "land", "here", "must", "big", "high", "such", "follow", "act", "why", "ask", "men", "change", "went", "light", "kind", "off", "need", "house", "picture", "try", "us", "again", "animal", "point", "mother", "world", "near", "build", "self", "earth", "father", "head", "stand", "own", "page", "should", "country", "found", "answer", "school", "grow", "study", "still", "learn", "plant", "cover", "food", "sun", "four", "thought", "let", "keep", "eye", "never", "last", "door", "between", "city", "tree", "cross", "since", "hard", "start", "might", "story", "saw", "far", "sea", "draw", "left", "late", "run", "don't", "while", "press", "close", "night", "real", "life", "few", "stop", "open", "seem", "together", "next", "white", "children", "begin", "got", "walk", "example", "ease", "paper", "often", "always", "music", "those", "both", "mark", "book", "letter", "until", "mile", "river", "car", "feet", "care", "second", "group", "carry", "took", "rain", "eat", "room", "friend", "began", "idea", "fish", "mountain", "north", "once", "base", "hear", "horse", "cut", "sure", "watch", "color", "face", "wood", "main", "enough", "plain", "girl", "usual", "young", "ready", "above", "ever", "red", "list", "though", "feel", "talk", "bird", "soon", "body", "dog", "family", "direct", "pose", "leave", "song", "measure", "state", "product", "black", "short", "numeral", "class", "wind", "question", "happen", "complete", "ship", "area", "half", "rock", "order", "fire", "south", "problem", "piece", "told", "knew", "pass", "farm", "top", "whole", "king", "size", "heard", "best", "hour", "better", "TRUE", "during", "hundred", "am", "remember", "step", "early", "hold", "west", "ground", "interest", "reach", "fast", "five", "sing", "listen", "six", "table", "travel", "less", "morning", "ten", "simple", "several", "vowel", "toward", "war", "lay", "against", "pattern", "slow", "center", "love", "person", "money", "serve", "appear", "road", "map", "science", "rule", "govern", "pull", "cold", "notice", "voice", "fall", "power", "town", "fine", "certain", "fly", "unit", "lead", "cry", "dark", "machine", "note", "wait", "plan", "figure", "star", "box", "noun", "field", "rest", "correct", "able", "pound", "done", "beauty", "drive", "stood", "contain", "front", "teach", "week", "final", "gave", "green", "oh", "quick", "develop", "sleep", "warm", "free", "minute", "strong", "special", "mind", "behind", "clear", "tail", "produce", "fact", "street", "inch", "lot", "nothing", "course", "stay", "wheel", "full", "force", "blue", "object", "decide", "surface", "deep", "moon", "island", "foot", "yet", "busy", "test", "record", "boat", "common", "gold", "possible", "plane", "age", "dry", "wonder", "laugh", "thousand", "ago", "ran", "check", "game", "shape", "yes", "hot", "miss", "brought", "heat", "snow", "bed", "bring", "sit", "perhaps", "fill", "east", "weight", "language", "among"
 ];
@@ -8,14 +11,38 @@ window.addEventListener('DOMContentLoaded', (event) => {
     inputField.focus();
 
 });
+
     document.addEventListener('keydown', (event) => {
-    if (event.key === ' ') {
-    spacebar();
-            }
+    if (event.target === input_div && (event.key === " " || event.code === "Space"))  {
+
+        
+        event.stopPropagation(); // Stop event propagation
+        event.preventDefault();
+
+
+
+        
+
+        
+        
+        
+        
+        
+
+
+
+
+}
 });
 document.addEventListener('keyup', (event) => {
-    if (event.key === ' ') {
-         input_div.focus() ;
+    // clearTimeout(1000);
+    if (event.key == ' ') {
+        
+        moveTextToCenter();
+        spacebar();
+        swapper();
+        input_div.focus() ;
+        
           }
 });
 function clearInputAndShowPlaceholder() {
@@ -39,20 +66,86 @@ i = Math.floor(Math.random() * (500));
 word4 = top1000Words[i];
 i = Math.floor(Math.random() * (500));
 word5 = top1000Words[i];
+function moveTextToLeft() {
+
+
+
+          
+    rightSideDiv.classList.add('move-left');
+    leftSideDiv.classList.add('move-left');
 
 
 
 
+
+
+
+
+    }
+
+
+function moveTextToCenter() {
+    rightSideDiv.classList.add('move-right');
+    leftSideDiv.classList.add('move-right');
+
+
+
+    
+            rightSideDiv.addEventListener('animationend', function() {
+                rightSideDiv.classList.remove('move-right');
+                // rightSideDiv.style.transform = 'none';
+            }, { once: true });
+            leftSideDiv.addEventListener('animationend', function() {
+                leftSideDiv.classList.remove('move-right');
+                // leftSideDiv.style.transform = 'none';
+            }, { once: true });
+
+  
+
+        
+        console.log("mtl");
+        rightSideDiv.addEventListener('animationend', function() {
+            rightSideDiv.classList.remove('move-left');
+        }, { once: true });
+        leftSideDiv.addEventListener('animationend', function() {
+            leftSideDiv.classList.remove('move-left');
+        }, { once: true });
+
+
+    // leftSideDiv.removeEventListener('transitionend', transitionEndHandler);
+    // rightSideDiv.removeEventListener('transitionend', transitionEndHandler);
+
+
+}
+
+function swapper(){
+
+    
+    function executeFunctionsWithDelay() {
+
+        setTimeout(() => {
+            document.getElementById("side-div left-side").innerText= helloarray[1];
+            document.getElementById("side-div right-side").innerText= helloarray[3];
+
+
+
+            }, 250); 
+        }
+
+        executeFunctionsWithDelay();
+    
+    input_div.placeholder = helloarray[2] ;
+
+}
 
 let helloarray = [word1,word2,word3,word4,word5]
 
 function spacebar() {
 
+
+    // moveTextToLeft();
     clearInputAndShowPlaceholder();
 
-    document.getElementById("side-div right-side").innerText= helloarray[3];
-    input_div.placeholder = helloarray[2] ;
-    document.getElementById("side-div left-side").innerText= helloarray[1];
 
 
 
@@ -61,6 +154,13 @@ function spacebar() {
     helloarray.shift();
     i = Math.floor(Math.random() * (500));
     helloarray.push(top1000Words[i])
+
+
+
+
+
+
+    
 
     
 }
